29dd7dbf2e61966f1db0e6bf46077282cbbb3297
Pranav0024 <39133940+Pranav0024@users.noreply.github.com>
2023-12-04 17:52:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', function() {
-  const menuIcon = document.querySelector('.menu-icon');
-  const menu = document.querySelector('.menu');
-
-  // Toggle the active class on the menu when the menu icon is clicked
-  menuIcon.addEventListener('click', function() {
-    menu.classList.toggle('active');
-  });
-});
+function redirectToClass() {
+  window.location.href = '#info';
+}
